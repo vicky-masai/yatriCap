73c68642338ee7d68e3946b0ff750de48feba2f9
Vicky Kumar <110033953+vicky-masai@users.noreply.github.com>
2023-05-05 14:55:21 +0530

    add driver frontend + backend conected and working

--- frontend/index.js
+++ frontend/index.js
@@ -1,3 +1,6 @@
+// form data 
+
+
 let ar = document.querySelector(".fa-angle-right");
 ar.addEventListener("click",()=>{
 
@@ -13,4 +16,61 @@ ar.addEventListener("click",()=>{
     ar.setAttribute("class","fa fa-angle-right");
     ar.style.color="black";
     }
+});
+
+
+// popup show and hide 
+let show = document.querySelector("#show");
+
+let addDriver = document.querySelector("#adddrv");
+addDriver.addEventListener("click",()=>{
+  show.style.display="block";
+
 })
+    
+// Add driver code [POST method]
+const form = document.querySelector('form');
+let api = "http://localhost:8080"
+
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
+  let location = document.querySelector("#location").value;
+  let driverName = document.querySelector("#driverName").value;
+  let mobileNumber = document.querySelector("#mobileNumber").value;
+  let licenceNumber = document.querySelector("#licenceNumber").value;
+  let aadharNumber = document.querySelector("#aadharNumber").value;
+  let driverPhoto = document.querySelector("#driverPhoto").value;
+
+  const formData = {
+    location,
+    driverName,
+    mobileNumber,
+    licenceNumber,
+    aadharNumber,
+    driverPhoto
+  }
+
+  document.querySelector("#loader").style.display="flex";
+  try {
+    const response = await fetch(`${api}/drivers/`, {
+      method: 'POST',
+      body: JSON.stringify(formData),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+  document.querySelector("#loader").style.display="none";
+  window.location.reload();
+});
+
+
+document.querySelector(".close").addEventListener("click",()=>{
+  show.style.display="none";
+})
+
+
